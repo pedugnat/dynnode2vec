23e2c1fdd09698ea6ca40e65a5a894396ec6b9fe
Paul-Emile Dugnat <paulemile.dugnat.contractor@axa-im.com>
2022-07-27 17:23:08 +0200

    fix hooks

---
+++ dynnode2vec/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""dynnode2vec is a package to embed dynamic graphs"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
