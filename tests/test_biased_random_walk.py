2d90a4a29e9da41374b9121549b5fddcfe1f165a
pedugnat <pedugnat@gmail.com>
2022-08-08 18:28:35 +0200

    pylinted tests

--- tests/test_biased_random_walk.py
+++ tests/test_biased_random_walk.py
@@ -1,3 +1,7 @@
+"""
+Test the BiasedRandomWalk class
+"""
+# pylint: disable=missing-function-docstring
 import random
 
 import numpy as np
@@ -6,27 +10,27 @@ import pytest
 import dynnode2vec
 
 
-@pytest.fixture
-def graphs():
+@pytest.fixture(name="graphs")
+def fixture_graphs():
     return dynnode2vec.utils.generate_dynamic_graphs(
         n_base_nodes=30, n_steps=5, base_density=0.02
     )
 
 
 def test_init(graphs):
-    BRW = dynnode2vec.biased_random_walk.BiasedRandomWalk(graphs[0])
+    brw = dynnode2vec.biased_random_walk.BiasedRandomWalk(graphs[0])
 
     # make sure nodes ids were converted to integers
-    assert list(BRW.graph.nodes()) == list(range(BRW.graph.number_of_nodes()))
+    assert list(brw.graph.nodes()) == list(range(brw.graph.number_of_nodes()))
 
 
 def test_weighted_choice(graphs):
-    BRW = dynnode2vec.biased_random_walk.BiasedRandomWalk(graphs[0])
-    rn = random.Random(0)
+    brw = dynnode2vec.biased_random_walk.BiasedRandomWalk(graphs[0])
+    rng = random.Random(0)
     eps = 0.02
     n_try = 1_000
 
-    choices = [BRW.weighted_choice(rn, np.array([1, 4, 4])) for _ in range(n_try)]
+    choices = [brw.weighted_choice(rng, np.array([1, 4, 4])) for _ in range(n_try)]
 
     assert all(choice in [0, 1, 2] for choice in choices)
 
@@ -39,40 +43,43 @@ def test_weighted_choice(graphs):
 @pytest.mark.parametrize("iq", [1.0, 2.0])
 @pytest.mark.parametrize("weighted", [True, False])
 def test_generate_walk(graphs, ip, iq, weighted):
+    # pylint: disable=invalid-name
     # make sure that tested node has at least one neighbor
-    G = graphs[0]
-    G.add_edge(0, 1, weight=0.5)
+    graph = graphs[0]
+    graph.add_edge(0, 1, weight=0.5)
 
     # add random weights to the graph for the weighted case
     if weighted:
-        for _, _, w in G.edges(data=True):
-            w["weight"] = random.random()
+        for _, _, data in graph.edges(data=True):
+            data["weight"] = random.random()
 
-    BRW = dynnode2vec.biased_random_walk.BiasedRandomWalk(G)
-    rn = random.Random(0)
+    brw = dynnode2vec.biased_random_walk.BiasedRandomWalk(graph)
+    rng = random.Random(0)
 
-    walk = BRW._generate_walk(
-        node=0, walk_length=10, ip=ip, iq=iq, weighted=weighted, rn=rn
+    # pylint: disable=protected-access
+    walk = brw._generate_walk(
+        node=0, walk_length=10, ip=ip, iq=iq, weighted=weighted, rn=rng
     )
 
     assert isinstance(walk, list)
-    assert all(n in BRW.graph.nodes() for n in walk)
+    assert all(n in brw.graph.nodes() for n in walk)
 
 
 @pytest.mark.parametrize("p", [0.5, 1.0])
 @pytest.mark.parametrize("q", [1.0, 2.0])
 @pytest.mark.parametrize("weighted", [True, False])
 def test_run(graphs, p, q, weighted):
-    G = graphs[0]
+    # pylint: disable=invalid-name
+    graph = graphs[0]
 
     # add random weights to the graph for the weighted case
     if weighted:
-        for *_, w in G.edges(data=True):
-            w["weight"] = random.random()
+        for *_, data in graph.edges(data=True):
+            data["weight"] = random.random()
 
-    BRW = dynnode2vec.biased_random_walk.BiasedRandomWalk(G)
+    brw = dynnode2vec.biased_random_walk.BiasedRandomWalk(graph)
 
-    random_walks = BRW.run(G.nodes(), p=p, q=q, weighted=weighted)
+    random_walks = brw.run(graph.nodes(), p=p, q=q, weighted=weighted)
 
     assert all(isinstance(walk, list) for walk in random_walks)
-    assert all(n in BRW.graph.nodes() for walk in random_walks for n in walk)
+    assert all(n in brw.graph.nodes() for walk in random_walks for n in walk)
--- tests/test_dynnode2vec.py
+++ tests/test_dynnode2vec.py
@@ -1,4 +1,7 @@
-from collections import namedtuple
+"""
+Test the DynNode2Vec class
+"""
+# pylint: disable=missing-function-docstring
 
 import gensim
 import networkx as nx
@@ -7,51 +10,52 @@ import pytest
 import dynnode2vec
 
 
-@pytest.fixture
-def graphs():
+@pytest.fixture(name="graphs")
+def fixture_graphs():
     return dynnode2vec.utils.generate_dynamic_graphs(
         n_base_nodes=30, n_steps=5, base_density=0.02
     )
 
 
-@pytest.fixture
+@pytest.fixture(name="dynnode2vec_object")
 def dynnode2vec_fixture():
     return dynnode2vec.DynNode2Vec(
         n_walks_per_node=5, walk_length=5, parallel_processes=1
     )
 
 
-@pytest.fixture
+@pytest.fixture(name="parallel_dynnode2vec_object")
 def dynnode2vec_parallel_fixture():
     return dynnode2vec.DynNode2Vec(
         n_walks_per_node=5, walk_length=5, parallel_processes=2
     )
 
 
-@pytest.fixture
+@pytest.fixture(name="node2vec_object")
 def plain_node2vec_parallel_fixture():
     return dynnode2vec.DynNode2Vec(
         n_walks_per_node=5, walk_length=5, plain_node2vec=True
     )
 
 
-def test_initialize_embeddings(graphs, dynnode2vec_fixture):
-    init_model, init_embeddings = dynnode2vec_fixture._initialize_embeddings(graphs)
+def test_initialize_embeddings(graphs, dynnode2vec_object):
+    # pylint: disable=protected-access
+    init_model, init_embeddings = dynnode2vec_object._initialize_embeddings(graphs)
 
     assert isinstance(init_model, gensim.models.Word2Vec)
     assert isinstance(init_embeddings[0], dynnode2vec.Embedding)
 
 
-def test_get_delta_nodes(graphs, dynnode2vec_fixture):
+def test_get_delta_nodes(graphs, dynnode2vec_object):
     current, previous = graphs[1], graphs[0]
 
-    delta_nodes = dynnode2vec_fixture.get_delta_nodes(current, previous)
+    delta_nodes = dynnode2vec_object.get_delta_nodes(current, previous)
 
     assert isinstance(delta_nodes, set)
     assert delta_nodes.issubset(current.nodes)
 
 
-def test_get_delta_nodes2(dynnode2vec_fixture):
+def test_get_delta_nodes2(dynnode2vec_object):
     previous = nx.complete_graph(n=4)
 
     current = previous.copy()
@@ -59,47 +63,45 @@ def test_get_delta_nodes2(dynnode2vec_fixture):
     current.add_edge(0, 5)
     current.remove_edge(1, 3)
 
-    delta_nodes = dynnode2vec_fixture.get_delta_nodes(current, previous)
+    delta_nodes = dynnode2vec_object.get_delta_nodes(current, previous)
 
     assert delta_nodes == {0, 1, 3, 5}
 
 
-def test_generate_updated_walks(graphs, dynnode2vec_fixture):
+def test_generate_updated_walks(graphs, dynnode2vec_object):
     current, previous = graphs[1], graphs[0]
 
-    updated_walks = dynnode2vec_fixture.generate_updated_walks(current, previous)
+    updated_walks = dynnode2vec_object.generate_updated_walks(current, previous)
 
     assert isinstance(updated_walks, list)
     assert all(node in current.nodes() for walk in updated_walks for node in walk)
 
 
-def test_node2vec_generate_updated_walks(graphs, plain_node2vec_parallel_fixture):
+def test_node2vec_generate_updated_walks(graphs, node2vec_object):
     current, previous = graphs[1], graphs[0]
 
-    updated_walks = plain_node2vec_parallel_fixture.generate_updated_walks(
-        current, previous
-    )
+    updated_walks = node2vec_object.generate_updated_walks(current, previous)
 
     assert isinstance(updated_walks, list)
     assert all(node in current.nodes() for walk in updated_walks for node in walk)
 
 
-def test_compute_embeddings(graphs, dynnode2vec_fixture):
-    embeddings = dynnode2vec_fixture.compute_embeddings(graphs)
+def test_compute_embeddings(graphs, dynnode2vec_object):
+    embeddings = dynnode2vec_object.compute_embeddings(graphs)
 
     assert isinstance(embeddings, list)
     assert all(isinstance(emb, dynnode2vec.Embedding) for emb in embeddings)
 
 
-def test_parallel_compute_embeddings(graphs, dynnode2vec_parallel_fixture):
-    embeddings = dynnode2vec_parallel_fixture.compute_embeddings(graphs)
+def test_parallel_compute_embeddings(graphs, parallel_dynnode2vec_object):
+    embeddings = parallel_dynnode2vec_object.compute_embeddings(graphs)
 
     assert isinstance(embeddings, list)
     assert all(isinstance(emb, dynnode2vec.Embedding) for emb in embeddings)
 
 
-def test_node2vec_compute_embeddings(graphs, plain_node2vec_parallel_fixture):
-    embeddings = plain_node2vec_parallel_fixture.compute_embeddings(graphs)
+def test_node2vec_compute_embeddings(graphs, node2vec_object):
+    embeddings = node2vec_object.compute_embeddings(graphs)
 
     assert isinstance(embeddings, list)
     assert all(isinstance(emb, dynnode2vec.Embedding) for emb in embeddings)
