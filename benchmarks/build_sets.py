a664a8967a762afbc6605c8870b3e99c3b38c256
ewindels <etienne.windels@hec.edu>
2022-08-10 02:05:07 +0200

    add get_node2vec_embeddings

---
+++ benchmarks/build_sets.py
@@ -0,0 +1,18 @@
+"""
+Build training and test sets from dynamic graphs.
+"""
+import networkx as nx
+
+from dynnode2vec.dynnode2vec import Embedding, DynNode2Vec
+
+
+def get_node2vec_embeddings(graphs: list[nx.Graph], parameters: dict) -> list[Embedding]:
+    """
+    Build plain node2vec embeddings at each time step.
+    """
+    dynnode2vec_obj = DynNode2Vec(**parameters)
+    embeddings = []
+    for graph in graphs:
+        _, embedding = dynnode2vec_obj.get_node2vec_embeddings(graph)
+        embeddings.extend(embedding)
+    return embeddings
